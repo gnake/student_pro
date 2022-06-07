19396862da42b3ee4297c837a94d6c575d2496b4
ggw <1498107871@qq.com>
2022-06-07 11:39:02 +0800

    选课控制类

---
+++ src/main/java/com/per/gnake/controller/DectiveController.java
@@ -0,0 +1,85 @@
+package com.per.gnake.controller;
+
+import com.per.gnake.bean.Dective;
+import com.per.gnake.bean.Teacher;
+import com.per.gnake.bean.Vo.CourseVo1;
+import com.per.gnake.service.DectiveService;
+import com.per.gnake.service.TeacherService;
+import org.apache.ibatis.javassist.expr.NewArray;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/dectiveController")
+public class DectiveController {
+
+    @Autowired
+    private DectiveService dectiveService;
+    @Autowired
+    private TeacherService teacherService;
+
+
+    //学生选课
+    @ResponseBody
+    @RequestMapping("/pickCourse")
+    public String pickCourse(CourseVo1 courseVo1) {
+//        System.out.println(courseVo1);
+
+        String sno = courseVo1.getSno();
+        String cno = courseVo1.getCno();
+        String tname = courseVo1.getTname();
+        Teacher teacher = teacherService.queryTeacherByName(tname);
+        String tno = teacher.getTno();
+
+        Dective dective = new Dective();
+        dective.setSno(sno);
+        dective.setCno(cno);
+        dective.setTno(tno);
+
+        //前端不好判断，只可一报出异常，无法报出准确的已选课程无法再选
+        //判断学号选的课是否被选过
+        int IsSelect = dectiveService.IsSelectedCourse(dective);
+
+        if(IsSelect > 0) {
+            System.out.println("此学生 " + courseVo1.getCname() + " 的课不可重复选") ;
+            return "IsSelected";
+        } else {
+            int row = dectiveService.pickCourseRecord(dective);
+            if(row > 0) {
+//            System.out.println("选课成功");
+                return "success";
+            } else {
+                System.out.println("选课失败");
+                return "error";
+            }
+        }
+    }
+
+    //退选课程
+    @ResponseBody
+    @RequestMapping("/exitSelectCourse")
+    public String exitRaceCourse(CourseVo1 courseVo1) {
+
+        String sno = courseVo1.getSno();
+        String cno = courseVo1.getCno();
+        String tname = courseVo1.getTname();
+        Teacher teacher = teacherService.queryTeacherByName(tname);
+        String tno = teacher.getTno();
+
+        Dective dective = new Dective();
+        dective.setSno(sno);
+        dective.setCno(cno);
+        dective.setTno(tno);
+
+        int rows = dectiveService.DeleteCourse(dective);
+        if(rows > 0) {
+//            System.out.println("退选成功");
+            return "success";
+        } else {
+            System.out.println("退选失败");
+            return "error";
+        }
+    }
+}
